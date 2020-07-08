06db4540b07ee389e46e3c53fa5c78478bf251bc
Heres <heres@lt-357-vm10.localdomain>
2020-07-08 08:47:34 +0200

    python

--- test2/test.py
+++ test2/test.py
@@ -7,25 +7,14 @@
 #
 #print('\nInterest = %0.2f' %interest)
 
-import logging
 import grpc
+from mtm.geo.service import geo_service_pb2_grpc
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = route_guide_pb2_grpc.RouteGuideStub(channel)
-        print("-------------- GetFeature --------------")
-        guide_get_feature(stub)
-        print("-------------- ListFeatures --------------")
-        guide_list_features(stub)
-        print("-------------- RecordRoute --------------")
-        guide_record_route(stub)
-        print("-------------- RouteChat --------------")
-        guide_route_chat(stub)
-
+    channel = grpc.insecure_channel('10.111.1.201:8690')
+    stub = geo_service_pb2_grpc.GeoServiceStub(channel)
+    geographies = stub.GetGeographies("")
+    print('size = %d', geographies.geographies.size)
 
 if __name__ == '__main__':
-    logging.basicConfig()
     run()
